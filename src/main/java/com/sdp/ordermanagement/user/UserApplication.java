632c8414ed4dc055ad77e8384b2ae4622bc2fb9b
bharadwajanand <mab.anand@gmail.com>
2025-06-02 10:09:16 +0530

    sys out

--- src/main/java/com/sdp/ordermanagement/user/UserApplication.java
+++ src/main/java/com/sdp/ordermanagement/user/UserApplication.java
@@ -8,7 +8,7 @@ public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
-		System.out.println(" Test");
+		System.out.println(" Hello world");
 	}
 
 }
